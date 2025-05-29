4bacd3d3a89c506c756a552a1fc3327672bd23df
tkmbenn <benn.taykm@gmail.com>
2025-05-30 01:10:31 +0800

    Uploaded Sample Flask App
    The HTML files need to be in the same directory as the app.py in the following configuration:
    > templates
    >> home.html
    >> submit.html
    >> wrapper.html
    > app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/', methods = ['GET'])
+def home_page():
+    return render_template("home.html")
+
+@app.route('/submit', methods = ['POST'])
+def submit():
+    submitted_text = request.form.get("text")
+    if not submitted_text:
+        return redirect(request.url_root)
+    return render_template("submit.html", shown_text = submitted_text)
+
+if __name__ == '__main__':
+    app.run()
