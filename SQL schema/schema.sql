da8f69e34116f0ed1e5782ad1263bc8ad0172ad8
tkmbenn <benn.taykm@gmail.com>
2025-06-14 20:45:46 +0800

    Added auto_increment to submission ID, added mock values for students table

--- SQL schema/schema.sql
+++ SQL schema/schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE task (
 );
 
 CREATE TABLE submission (
-    sid VARCHAR(100) PRIMARY KEY,
+    sid VARCHAR(100) PRIMARY KEY AUTO_INCREMENT,
     tid VARCHAR(100),
     username VARCHAR(100),
     code VARCHAR(1000),
@@ -34,3 +34,11 @@ CREATE TABLE submission (
     FOREIGN KEY (tid) REFERENCES task(tid),
     FOREIGN KEY (username) REFERENCES session(username)
 );
+
+INSERT INTO students (username, password_hash)
+    VALUES
+    ('ben', '6edfe0531855295c5541d2666d604463'),
+    ('benn', 'ea2b11c7e6bc373c628be113847b039d'),
+    ('sasi', 'aa6bfe8bcf6eb51f7e158d8e5101fb71'),
+    ('tricia', '0b4d4ee0c8e3cda51616f1f6cc64a655'),
+    ('zongyu', '6e4a828704323be31656ec168be89920')s;
