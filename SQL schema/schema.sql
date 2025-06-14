a2bc0549e76180613aab9966df7c3237fe73ff39
tkmbenn <benn.taykm@gmail.com>
2025-06-14 20:04:58 +0800

    Updated references for all foreign keys

--- SQL schema/schema.sql
+++ SQL schema/schema.sql
@@ -18,16 +18,19 @@ CREATE TABLE assessment (
 
 CREATE TABLE task (
     tid VARCHAR(100) PRIMARY KEY,
-    aid VARCHAR(100) FOREIGN KEY,
-    title VARCHAR(200)
+    aid VARCHAR(100),
+    title VARCHAR(200),
+    FOREIGN KEY (aid) REFERENCES assessment(aid)
 );
 
 CREATE TABLE submission (
     sid VARCHAR(100) PRIMARY KEY,
-    tid VARCHAR(100) FOREIGN KEY,
-    username VARCHAR(100) FOREIGN KEY,
+    tid VARCHAR(100),
+    username VARCHAR(100),
     code VARCHAR(1000),
     attempt_no INT,
     score FLOAT(3),
-    submitted_at DATETIME(200)
+    submitted_at DATETIME(200),
+    FOREIGN KEY (tid) REFERENCES task(tid),
+    FOREIGN KEY (username) REFERENCES session(username)
 );
