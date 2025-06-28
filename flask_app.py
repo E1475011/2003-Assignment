bb5088b83dce72925ece0d05926e3a3d5736d976
E1475011 <E1475011@u.nus.edu>
2025-06-28 16:43:26 +0800

    Added title to link

--- flask_app.py
+++ flask_app.py
@@ -58,7 +58,7 @@ def login():
 @app.route('/submit', methods = ['GET'])
 def submit_select_question():
     questions = select_question_sql.get_all_questions()
-    return render_template("selectquestion.html", parameter = 'submit', questions = questions)
+    return render_template("selectquestion.html", parameter = 'submit', questions = questions[0], titles = questions[1])
 
 # Submit - After Select Question
 @app.route('/submitquestion', methods = ['GET'])
@@ -84,7 +84,7 @@ def submit():
 @app.route('/score', methods = ['GET'])
 def score_select_question():
     questions = select_question_sql.get_all_questions()
-    return render_template("selectquestion.html", parameter = 'score', questions = questions)
+    return render_template("selectquestion.html", parameter = 'score', questions = questions[0], titles = questions[1])
 
 # Score - After Select Question
 @app.route('/scorequestion', methods = ['GET'])
@@ -96,7 +96,7 @@ def score():
 @app.route('/leaderboard', methods = ['GET'])
 def leaderboard():
     questions = select_question_sql.get_all_questions()
-    return render_template("selectquestion.html", parameter = 'leaderboard', questions = questions)
+    return render_template("selectquestion.html", parameter = 'leaderboard', questions = questions[0], titles = questions[1])
 
 # Leaderboard - Select Question
 @app.route('/leaderboardquestion', methods = ['GET'])
