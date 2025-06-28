c9a55369b2dc70ac140b97b121c219147fa9e179
tkmbenn <benn.taykm@gmail.com>
2025-06-28 12:09:45 +0800

    test change password exception

--- flask_app.py
+++ flask_app.py
@@ -105,13 +105,13 @@ def change_password():
         )
         cursor = cnx.cursor()
         cursor.execute("SELECT s.username FROM students s, login_session l WHERE s.username = l.username AND l.session_id = %s",(session['number'],))
-        username = cursor.fetchall()[0][0]
-
-        if not username:
+        result_rows = cursor.fetchall()
+        if not result_rows:
             error = 'Password change was unsuccessful. Please try again.'
             cursor.close()
             cnx.close()
         else:
+            username = result_rows[0][0]
             cursor.execute("UPDATE students SET password_hash=%s WHERE password_hash=%s AND username=%s",(newpassword, oldpassword, username))
             cnx.commit()
             cursor.close()
