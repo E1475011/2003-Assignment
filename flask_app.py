83fc8e1800fc8ba6ed954c5926cadde57ad9317c
serenadesg <19544816+serenadesg@users.noreply.github.com>
2025-06-28 19:45:08 +0800

    updates flask app and score html
    flask - to add in new python script and score section
    score html - replacing old data

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ import mysql.connector
 from uuid import uuid4
 import hashlib
 import select_question_sql
+import get_all_scores
 import submission_grading
 import datetime
 import pandas
@@ -140,8 +141,21 @@ def score_select_question():
 # Score - After Select Question
 @app.route('/scorequestion', methods = ['GET'])
 def score():
-    question_no = request.args.get('question_no')
-    return render_template("score.html", question_no = question_no)
+    question_details = request.args.get('question_no')
+    #output: (1, 'Math Quiz 1', datetime.datetime(2025, 7, 1, 9, 0))
+    assessment_id = question_details[0]
+    task_id = question_details[1]
+
+
+    submission_details = get_all_scores.get_data_submission()
+    #   submission_id, aid, username, score, submitted_at
+    #output: (1,        1,   'ben',    0.85,   submit_time)
+
+    get_subAID = submission_details[1]
+    get_subscore = submission_details[3]
+
+    #final - pass a var to score page, with the data from get scores
+    return render_template("score.html", get_subAID = get_subAID, get_subscore = get_subscore)
 
 # Leaderboard
 @app.route('/leaderboard', methods = ['GET'])
