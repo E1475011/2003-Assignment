2ba4d5dba98b44fb3e3f737c1e29bc57a8af172a
BenLearnOutlook <E1475011@u.nus.edu>
2025-06-29 17:42:49 +0800

    Update changeduedate.sql

--- SQL schema/changeduedate.sql
+++ SQL schema/changeduedate.sql
@@ -1,11 +1,15 @@
 SELECT * FROM submission WHERE submitted_at BETWEEN '2025-07-01' AND '2025-08-01'; 
-<!--old and new due date-->
+--old and new due date
 
 UPDATE submission SET score = score / 9 * 10 WHERE submitted_at BETWEEN '2025-07-01' AND '2025-08-01';
-<!--duedate is push later-->
+--duedate is push later
 UPDATE submission SET score = score / 10 * 9 WHERE submitted_at BETWEEN '2025-07-01' AND '2025-08-01';
-<!--duedate is push earlier-->
-<!--show updated score-->
+--duedate is push earlier
+
+SELECT * FROM submission WHERE submitted_at BETWEEN '2025-07-01' AND '2025-08-01'; 
+--show updated score
 
 update assessment set due_date = '2025-07-04' WHERE aid = '1';
-<!-- to update duedate for specific assessment-->
+-- to update duedate for specific assessment
+SELECT * FROM assessment WHERE aid = '1';
+-- show due date updated
