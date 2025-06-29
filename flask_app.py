076abb0916631f2be361eeb715a660854446e3a5
BenLearnOutlook <E1475011@u.nus.edu>
2025-06-29 14:15:55 +0800

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -209,6 +209,8 @@ def leaderboard_select_question():
         )
     cursor = cnx.cursor()
     aid = int(question_parts[0][:-2])
+    print("aid", aid)
+    print("SELECT username, score FROM submission WHERE aid = %d ORDER BY score DESC LIMIT 5;",(aid,))
     cursor.execute("SELECT username, score FROM submission WHERE aid = %d ORDER BY score DESC LIMIT 5;",(aid,))
     topscorers = cursor.fetchall()
     cursor.close()
