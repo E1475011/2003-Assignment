0e371e39b21a0756253dafff29b5e15f707c72ce
E1475011 <E1475011@u.nus.edu>
2025-06-30 09:56:33 +0800

    Update schema.sql

--- SQL schema/schema.sql
+++ SQL schema/schema.sql
@@ -162,7 +162,7 @@ INSERT INTO parts (pid, tid, model_ans, query) VALUES
 
 (0, 13, "((2),(3),(4))", "SELECT order_id FROM orders;"),
 (0, 14, "((5))", "SELECT order_id FROM orders WHERE order_id = 5;"),
-(1, 14, "((5, 1, 3))", "SELECT order_id, inventory_id, amount FROM order_items WHERE order_id = 5, inventory_id = 1"),
+(1, 14, "((5, 1, 3))", "SELECT order_id, inventory_id, amount FROM order_items WHERE order_id = 5, inventory_id = 1;"),
 (2, 14, "((27))", "SELECT amount FROM inventory WHERE inventory_id = 1;");
 
 
