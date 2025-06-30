7185f6d10b9214bd3c2a0b7a4da7b6cb90469b8e
tkmbenn <benn.taykm@gmail.com>
2025-06-30 09:43:39 +0800

    debug

--- flask_app.py
+++ flask_app.py
@@ -67,8 +67,8 @@ def submit_select_question():
 @app.route('/submitquestion', methods = ['GET', 'POST'])
 def submit():
     if request.method == 'POST':
-        # debug_code = []
-        # debug_model = []
+        debug_code = []
+        debug_model = []
         counter = 1
         tid = []
         code = []
@@ -119,8 +119,8 @@ def submit():
                     # model
                     cursor.execute("SELECT model_ans FROM parts where tid = %s and pid = %s", (tid[task_idx], part_idx))
                     model_execute = cursor.fetchall()
-                    # debug_code.append(code_execute)
-                    # debug_model.append(model_execute)
+                    debug_code.append(code_execute)
+                    debug_model.append(model_execute)
                     grade = submission_grading.rs_similarity(code_execute, model_execute)
                     task_grade.append(grade)
                 else:
@@ -137,8 +137,8 @@ def submit():
                     # model
                     cursor.execute("SELECT model_ans FROM parts where tid = %s and pid = %s", (tid[task_idx], part_idx))
                     model_execute = cursor.fetchall()
-                    # debug_code.append(code_execute)
-                    # debug_model.append(model_execute)
+                    debug_code.append(code_execute)
+                    debug_model.append(model_execute)
                     grade = submission_grading.rs_similarity(code_execute, model_execute)
                     task_grade.append(grade)
             assessment_grade.append(sum(task_grade)/len(task_grade))
@@ -150,8 +150,8 @@ def submit():
         cnx1.close()
         cursor.close()
         cnx.close()
-        # return f'{debug_code}, {debug_model}'
-        return redirect('/home')
+        return f'{debug_code}, {debug_model}'
+        # return redirect('/home')
     
     # GET method - sample route: /submitquestion?question_no=(1, 'Math Quiz 1', datetime.datetime(2025, 7, 1, 9, 0))
 
